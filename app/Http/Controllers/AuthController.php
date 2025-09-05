2c05eb471cecca52f85c6a95d2b1f4f5aa1b94ea
elisioui ux <elisiouiux@gmail.com>
2025-09-05 03:08:34 +0100

    feat(auth): add authcontroler, register login e logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    use HttpResponses;
+
+    public function register(Request $request)
+    {
+        return response()->json('Meu metodo register');
+    }
+
+    public function login(Request $request)
+    {
+        return response()->json('Meu metodo login');
+    }
+
+    public function logout(Request $request)
+    {
+        return response()->json('Meu metodo logout');
+    }
 }
--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -2,22 +2,22 @@
 
 namespace App\Traits;
 
-trait HttpResponses {
-    protected function sucess($data, $message = null ,$code = 200)
+trait HttpResponses{
+    protected function sucess($data, $message = null, $code = 200)
     {
         return response()->json([
-            'status' => 'Request was sucessful',
+            'status' => 'Request was success',
             'message' => $message,
             'data' => $data
         ], $code);
-    } 
+    }
 
-    protected function error($data, $message = null ,$code)
+    protected function error($data, $message = null, $code = 400)
     {
         return response()->json([
-            'status' => 'Error has ocurred',
+            'status' => 'Ocurred an error',
             'message' => $message,
             'data' => $data
         ], $code);
-    } 
+    }
 }
