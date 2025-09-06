c9ab7a8f04019db3bdd2a8a1e719b56f3a2f1282
elisioui ux <elisiouiux@gmail.com>
2025-09-06 08:21:16 +0100

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,25 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;  
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function register(Request $request)
+
+    public function register(StoreUserRequest $request)
     {
-        return response()->json('Meu metodo register');
+        // Criar usuário
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Retorno com token de autenticação
+ return $this->success([
+    'id'    => $user->id,
+    'name'  => $user->name,
+    'email' => $user->email,
+    'token' => $user->createToken('Token do user ' . $user->name)->plainTextToken,
+]);
+
     }
 
     public function login(Request $request)
     {
-        return response()->json('Meu metodo login');
+        return response()->json('Meu método login');
     }
 
     public function logout(Request $request)
     {
-        return response()->json('Meu metodo logout');
+        return response()->json('Meu método logout');
     }
 }
--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -3,7 +3,7 @@
 namespace App\Traits;
 
 trait HttpResponses{
-    protected function sucess($data, $message = null, $code = 200)
+    protected function success($data, $message = null, $code = 200)
     {
         return response()->json([
             'status' => 'Request was success',
